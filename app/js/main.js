d167d2ddf5e0067c6a0587b70bd615f09bb19785
Michał Grochowski <mic.grochowski@gmail.com>
2017-11-15 11:37:08 +0100

    v0.21

--- app/js/main.js
+++ app/js/main.js
@@ -8,18 +8,18 @@
     const cityInput = document.getElementById("cityInput");
     const cityButton = document.getElementById("cityButton");
 
-    const urlCity = "api.openweathermap.org/data/2.5/weather?q=";
+    const api = "api.openweathermap.org/data/2.5/weather?q=";
     const key = "7c7fe6ea927aa8bfbd07cfce66100663";
 
     cityButton.addEventListener("click", function() {
-        fetch(urlCity + cityInput.value + "&APPID=" + key)
+        fetch(api + cityInput.value + "&APPID=" + key + "&units=metric")
         .then((resp) => resp.json())
         .then(function(data) {
-            temperature.innerHTML = data.main.temp;
+            // temperature.innerHTML = data.main.temp;
             console.log(data);
         })
         .catch(function(error) {
             console.log(error);
-        });
+        })
     });
 })();
