482cc8bf366d335826e8a8875b0a4a2b45ae2ea8
Michał Grochowski <mic.grochowski@gmail.com>
2017-11-28 14:24:12 +0100

    v1.0
    Added different icon based on weather condition and time

--- app/js/main.js
+++ app/js/main.js
@@ -6,25 +6,55 @@
     const pressure = document.getElementById("pressure");
     const windDir = document.getElementById("windDir");
     const clouds = document.getElementById("clouds");
+    const rain = document.getElementById("rain");
     const city = document.getElementById("city");
+    const tempMin = document.getElementById("tempMin");
+    const tempMax = document.getElementById("tempMax");
     const cityInput = document.getElementById("cityInput");
     const cityButton = document.getElementById("cityButton");
     const locate = document.getElementById("locate");
+    const mainIcon = document.getElementById("mainIcon");
+    const wiClass = new RegExp(/^wi-.+$/);
     // API link and key
     const api = "https://api.openweathermap.org/data/2.5/weather?";
     const key = "7c7fe6ea927aa8bfbd07cfce66100663";
     // Function that displays current weather based on user provided localisation
     cityButton.addEventListener("click", function() {
+        if (mainIcon.classList.contains(new RegExp(/^wi-.+$/))) {
+            console.log("Ikonka zawiera wi-");
+        }
         fetch(api + "q=" + cityInput.value + "&APPID=" + key + "&units=metric")
         .then((resp) => resp.json())
         .then((data) => {
+            let date = new Date();
+            let currentTime = date.getHours();
+            if (currentTime >= 6 && currentTime <= 18 && data.clouds.all < "25") {;
+                mainIcon.classList.add("wi-day-sunny");
+            } else if (currentTime >= 18 && currentTime <= 6 && data.clouds.all < "25") {
+                mainIcon.classList.add("wi-night-clear");
+            } else if (currentTime >= 6 && currentTime <= 18 && data.clouds.all >= "25" && data.clouds.all < "50") {
+                mainIcon.classList.add("wi-day-cloudy");
+            } else if (currentTime >= 18 && currentTime <= 6 && data.clouds.all >= "25" && data.clouds.all < "50") {
+                mainIcon.classList.add("wi-night-alt-cloudy");
+            } else if (currentTime >= 6 && currentTime <= 18 && data.clouds.all >= "50" && data.clouds.all < "75") {
+                mainIcon.classList.add("wi-day-cloudy-high");
+            } else if (currentTime >= 18 && currentTime <= 6 && data.clouds.all >= "50" && data.clouds.all < "75") {
+                mainIcon.classList.add("wi-night-alt-cloudy-high");
+            } else if (data.clouds.all >= "75") {
+                mainIcon.classList.add("wi-cloudy");
+            }
             temperature.innerHTML = data.main.temp;
+            tempMin.textContent = data.main.temp_min;
+            tempMax.textContent = data.main.temp_max;
             pressure.textContent = data.main.pressure + " hPa";
             humidity.textContent = data.main.humidity + " %";
             wind.textContent = data.wind.speed + " m/s";
             clouds.textContent = data.clouds.all + " %";
             windDir.textContent = data.wind.deg;
             city.textContent = data.name;
+            if (data.rain !== undefined) {
+                rain.textContent = data.rain + " %";
+            }
         })
         .catch(function(error) {
             console.log(error);
@@ -32,17 +62,42 @@
     });
     // Function that displays current weather based on user geolocation
     locate.addEventListener("click", function() {
+        if (mainIcon.classList.contains(wiClass)) {
+            console.log("Ikonka zawiera wi-");
+        }
         navigator.geolocation.getCurrentPosition(function(position) {
             fetch(api + "lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=" + key + "&units=metric")
             .then((resp) => resp.json())
             .then((data) => {
+                let date = new Date();
+                let currentTime = date.getHours();
+                if (currentTime >= 6 && currentTime <= 18 && data.clouds.all < "25") {;
+                    mainIcon.classList.add("wi-day-sunny");
+                } else if (currentTime >= 18 && currentTime <= 6 && data.clouds.all < "25") {
+                    mainIcon.classList.add("wi-night-clear");
+                } else if (currentTime >= 6 && currentTime <= 18 && data.clouds.all >= "25" && data.clouds.all < "50") {
+                    mainIcon.classList.add("wi-day-cloudy");
+                } else if (currentTime >= 18 && currentTime <= 6 && data.clouds.all >= "25" && data.clouds.all < "50") {
+                    mainIcon.classList.add("wi-night-alt-cloudy");
+                } else if (currentTime >= 6 && currentTime <= 18 && data.clouds.all >= "50" && data.clouds.all < "75") {
+                    mainIcon.classList.add("wi-day-cloudy-high");
+                } else if (currentTime >= 18 && currentTime <= 6 && data.clouds.all >= "50" && data.clouds.all < "75") {
+                    mainIcon.classList.add("wi-night-alt-cloudy-high");
+                } else if (data.clouds.all >= "75") {
+                    mainIcon.classList.add("wi-cloudy");
+                }
                 temperature.innerHTML = data.main.temp;
+                tempMin.innerHTML = data.main.temp_min + " &deg; C";
+                tempMax.innerHTML = data.main.temp_max + " &deg; C";
                 pressure.textContent = data.main.pressure + " hPa";
                 humidity.textContent = data.main.humidity + " %";
                 wind.textContent = data.wind.speed + " m/s";
                 clouds.textContent = data.clouds.all + " %";
                 windDir.textContent = data.wind.deg;
                 city.textContent = data.name;
+                if (data.rain !== undefined) {
+                    rain.textContent = data.rain + " %";
+                }
             })
             .catch(function(error) {
                 console.log(error);
